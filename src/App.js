9b81cff2dea74f26ad834bcfd376fc62ff476008
ahmetcng33 <ahmetcingi333@gmail.com>
2022-10-28 14:12:11 +0300

    14.12

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
 import './App.scss'
-function App() {
-  return (
+
+const App = () => {
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to react</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque inventore laborum eligendi a quaerat dolor cum, veniam aperiam modi accusantium quibusdam, ad culpa magnam exercitationem provident magni et voluptates illum?</p>
     </div>
-  );
+  )
 }
 
 export default App;
