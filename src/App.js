e696f1030739e83c0afc7b564171a96fdf359e53
ahmetcng33 <ahmetcingi333@gmail.com>
2022-10-28 15:59:01 +0300

    navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.scss'
+import Navbar from './components/Navbar/Navbar';
 
 const App = () => {
   return(
     <div className="App">
-      <h1>Welcome to react</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque inventore laborum eligendi a quaerat dolor cum, veniam aperiam modi accusantium quibusdam, ad culpa magnam exercitationem provident magni et voluptates illum?</p>
+      <Navbar/>
     </div>
   )
 }
