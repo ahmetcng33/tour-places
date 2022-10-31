16e5b308dfa7cba659f51345e81d0ef260fae8e8
ahmetcng33 <ahmetcingi333@gmail.com>
2022-10-31 14:04:22 +0300

    card denemesi

---
+++ src/components/Main/Card.jsx
@@ -0,0 +1,11 @@
+const Card = (data) => {
+    console.log("ne geliyor", data);
+    return(
+        <div className="cards">
+            <h1>Cards</h1>
+        </div>
+
+    )
+}
+
+export default Card;
